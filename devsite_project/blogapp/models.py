58934c8ecf38576e8310c8ad630edf55207da168
Miles Ford <milesford@Miless-MacBook-Pro.local>
2021-03-20 12:09:54 -0400

    put date in model for post order

--- devsite_project/blogapp/models.py
+++ devsite_project/blogapp/models.py
@@ -6,8 +6,11 @@ class Blogpost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     photo = models.ImageField(upload_to='photos')
+    date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['-date',]
    
         
